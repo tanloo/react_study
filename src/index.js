5372e111f218c3d51feecb12b785dd9cafe11c55
tlzwd135 <tlzwd135@163.com>
2019-03-03 21:19:41 +0800

    6.2

--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
 
-class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {date: new Date()};
-    }
-    render() {
-        return (
-            <div>
-                <h1>Hello, world!</h1>
-                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-            </div>
-        );
-    }
-}
-
-function tick() {
-    ReactDOM.render(
-        <Clock />,
-        document.getElementById('root')
-    );
-}
-
-setInterval(tick, 1000);
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+);
