cbcd960ed43e9569725d918131e31cb00929dc5e
tanloo <zxc5095@gmail.com>
2019-04-20 22:43:12 +0800

    modified:   package.json 	modified:   public/index.html 	modified:   src/index.js 	src/app.js 	src/component/ 	src/modules/ 	src/routes.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Provider} from 'react-redux';
+import {createStore} from "redux";
+import routes from "./routes";
+
 
 ReactDOM.render(
-    <App />,
+    <Provider store={createStore()}>
+        {routes}
+    </Provider>,
     document.getElementById("root")
 );
